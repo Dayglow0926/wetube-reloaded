1fe90d2b5ee5b7e373827fdb329d67feae80c65e
dayglow0926 <dayglow0926@gmail.com>
2022-09-22 17:40:26 +0900

    feat: router에서 파생된 controller 학습 및 분리

---
+++ src/controller/userController.js
@@ -0,0 +1,8 @@
+export const join = (req, res) => res.send("user join");
+export const login = (req, res) => res.send("user login");
+export const logout = (req, res) => res.send("user logout");
+
+export const edit = (req, res) => res.send("user edit");
+export const deleteInfo = (req, res) => res.send("user delete");
+
+export const see = (req, res) => res.send("user profile");
---
+++ src/controller/videoController.js
@@ -0,0 +1,6 @@
+export const trending = (req, res) => res.send("trending video Home");
+export const search = (req, res) => res.send("video search");
+export const see = (req, res) => res.send("video watch");
+export const edit = (req, res) => res.send("video edit");
+export const remove = (req, res) => res.send("video remove");
+export const upload = (req, res) => res.send("video upload");
