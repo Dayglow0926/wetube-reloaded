9c3d7c3cd502c4d3374a187d4f9b8c2d80daccca
dayglow0926 <dayglow0926@gmail.com>
2022-09-23 23:17:04 +0900

    fix: parameter 위치 변경
    파라미터 url 을 맨 위에 둘 경우 upload 같은 url 또한 id로 인식하여 접근이 불가

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 import { edit, deleteInfo, see, logout } from "../controller/userController";
 
 const userRouter = express.Router();
-userRouter.get("/:id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", deleteInfo);
 userRouter.get("/logout", logout);
+userRouter.get("/:id", see);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 import { see, edit, remove, upload } from "../controller/videoController";
 
 const videoRouter = express.Router();
+videoRouter.get("/upload", upload);
 videoRouter.get("/:id", see);
 videoRouter.get("/:id/edit", edit);
 videoRouter.get("/:id/delete", remove);
-videoRouter.get("/upload", upload);
 
 export default videoRouter;
