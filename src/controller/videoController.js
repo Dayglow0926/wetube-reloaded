81a9cc0ececa5a82bc118546a2785c1e7496ebb7
dayglow0926 <dayglow0926@gmail.com>
2022-09-26 23:33:15 +0900

    fix: pug 사용을 위한 response render 사용

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,8 +1,6 @@
-export const trending = (req, res) => res.send("trending video Home");
+export const trending = (req, res) => res.render("home");
 export const search = (req, res) => res.send("video search");
-export const see = (req, res) => {
-  res.send(`video watch ${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("video edit");
 export const remove = (req, res) => res.send("video remove");
 export const upload = (req, res) => res.send("video upload");
