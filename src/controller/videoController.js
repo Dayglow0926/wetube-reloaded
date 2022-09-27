b170ce512ca396ae39ad0c9bfa25a8971c4f7563
dayglow0926 <dayglow0926@gmail.com>
2022-09-27 23:34:56 +0900

    refactor: 반복되는 코드를 수정
    pug 에서 반복되는 코드를 inheritance (상속)을 통해 해결
    
    1. #{변수}를 사용하여 반복되는 문자를 최소화
    변수는 controller 에서 render 를 통해 전달
    
    2. block 과 extends 를 통해 base layout 을 만들어 기본적인 틀을 반복하지 않도록함
    extends 를 통해 base.pug 를 호출
    block 을 통해 원하는 코드를 삽입

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,6 +1,6 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
 export const search = (req, res) => res.send("video search");
-export const see = (req, res) => res.render("watch");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.send("video edit");
 export const remove = (req, res) => res.send("video remove");
 export const upload = (req, res) => res.send("video upload");
