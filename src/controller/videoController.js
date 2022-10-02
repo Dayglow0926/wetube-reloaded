b12e20f80429bf14ee69ddadde5bd0ef87282c4a
dayglow0926 <dayglow0926@gmail.com>
2022-10-02 17:33:43 +0900

    feat: mixin 학습
    똑똑한 paritial 데이터를 가져올 수 있어서 component 를 만들 수 있다.

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,5 +1,30 @@
 export const trending = (req, res) => {
-  const videos = [];
+  const videos = [
+    {
+      title: "first Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "5 minutes age",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Seconds Video",
+      rating: 4,
+      comments: 5,
+      createdAt: "3 minutes age",
+      views: 49,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 2,
+      comments: 1,
+      createdAt: "1 minutes age",
+      views: 50,
+      id: 3,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => res.send("video search");
