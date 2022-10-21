6f31a08f3f7cd70cdfee0afc127893e12c299618
Dayglow0926 <dayglow0926@gmail.com>
2022-10-21 18:13:55 +0900

    fix: url 매핑 변경 및 watch 페이지 db 조회 추가

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -7,10 +7,11 @@ export const home = async (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const search = (req, res) => res.send("video search");
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
   const { id } = req.params;
-
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  console.log(video);
+  return res.render("watch", { pageTitle: video.title, video });
 };
 
 export const getEdit = (req, res) => {
