165b69b84a8bf22b518a670c105b49d219851ef4
Dayglow0926 <dayglow0926@gmail.com>
2022-11-20 19:03:57 +0900

    feat: login, join 페이지 구현

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,5 +1,71 @@
-export const join = (req, res) => res.send("user join");
-export const login = (req, res) => res.send("user login");
+import { response } from "express";
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle = "Join";
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+
+  if (password !== password2) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+
+  if (exists) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
+export const getLogin = (req, res) => res.render("login");
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+
+  const pageTitle = "Login";
+
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An accout with this username does not exists.",
+    });
+  }
+
+  const ok = await bcrypt.compare(password, user.password);
+  if (!ok) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An accout with this password does not exists.",
+    });
+  }
+
+  req.session.loggedIn = true;
+  req.session.user = user;
+
+  return res.redirect("/");
+};
 export const logout = (req, res) => res.send("user logout");
 
 export const edit = (req, res) => res.send("user edit");
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import logger from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const loggerMiddleware = logger("dev");
@@ -13,7 +16,25 @@ app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
 app.use(loggerMiddleware);
 
-app.use("/", globalRouter);
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
+  })
+);
+
+// app.use((req, res, next) => {
+//   req.sessionStore.all((error, sessions) => {
+//     console.log(sessions);
+//     next();
+//   });
+// });
+
+app.use(localsMiddleware);
+
+app.use("/", rootRouter);
 app.use("/video", videoRouter);
 app.use("/users", userRouter);
 
