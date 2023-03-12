96a08ff0fc70ab1f2d46b5a4082ca6debec0ecad
Dayglow0926 <dayglow0926@gmail.com>
2023-03-13 04:12:42 +0900

    feat: aws s3 환경 설정 추가

--- src/server.js
+++ src/server.js
@@ -15,8 +15,19 @@ const loggerMiddleware = logger("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(flash());
+// app.use((req, res, next) => {
+//   res.header("Cross-Origin-Embedder-Policy", "require-corp");
+//   res.header("Cross-Origin-Opener-Policy", "same-origin");
+//   next();
+// });
+
 app.use((req, res, next) => {
-  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Cross-Origin-Embedder-Policy", "credentialless");
   res.header("Cross-Origin-Opener-Policy", "same-origin");
   next();
 });
